f2dd2fd8b608b699bb02aaa72edaa885bf33783b
Metodi Petrovski <149816710+MetodiPetrovski@users.noreply.github.com>
2026-06-23 21:13:36 +0200

    Update search.py

--- retrieval/search.py
+++ retrieval/search.py
@@ -1,1 +1,28 @@
+def search_repo(
+    question,
+    model,
+    index,
+    embedding_docs,
+    k=5
+):
+    query_embedding = model.encode(
+        [question],
+        convert_to_numpy=True
+    ).astype("float32")
 
+    faiss.normalize_L2(query_embedding)
+
+    scores, indices = index.search(
+        query_embedding,
+        k
+    )
+
+    results = []
+
+    for score, idx in zip(scores[0], indices[0]):
+        results.append({
+            "score": float(score),
+            "text": embedding_docs[idx]
+        })
+
+    return results
