7f1b8afa05f32df65244d06bdb846d240fadc90e
Metodi Petrovski <metopetrovski13@gmail.com>
2026-08-22 17:56:04 +0200

    add commit support to repository loader

--- evaluation/repos.py
+++ evaluation/repos.py
@@ -9,6 +9,7 @@ REPOSITORIES = [
   {
         "name": "strix",
         "url": "https://github.com/usestrix/strix.git",
+        "commit": "8157ccba276c8fdd5eaa07a1a9d8d686315f6bd1"
         "benchmark": BASE_DIR / "benchmarks" / "strix.json",
     }
 ]
