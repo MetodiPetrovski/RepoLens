e46821a8e13520c17377d56f505d2a9d3da64308
Metodi Petrovski <metopetrovski13@gmail.com>
2026-08-22 19:01:25 +0200

    improving git_loader

--- ingestion/git_loader.py
+++ ingestion/git_loader.py
@@ -5,6 +5,12 @@ import os
 def clone_repo(url, target_dir, commit=None):
 
     if os.path.exists(target_dir):
+
+        if commit:
+            repo = Repo(target_dir)
+            repo.git.checkout(commit)
+            return target_dir
+
         print("Directory already exists.")
         return target_dir
 
