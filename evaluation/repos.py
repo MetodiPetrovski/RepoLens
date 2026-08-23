f74a438751557abdf063f9227c6eed4098d77e83
Metodi Petrovski <metopetrovski13@gmail.com>
2026-08-24 00:41:43 +0200

    adding a new eval repo

--- evaluation/repos.py
+++ evaluation/repos.py
@@ -11,5 +11,13 @@ REPOSITORIES = [
         "url": "https://github.com/usestrix/strix.git",
         "commit": "1c499c5b2d788c553f0d276b389b2b424e483304",
         "benchmark": BASE_DIR / "benchmarks" / "strix.json",
+    },
+
+  {   "name": "requests",
+      "url": "https://github.com/psf/requests",
+      "commit": "8f8b212de8c2129d7954c6cd373762880375620a",
+      "benchmark": BASE_DIR / "benchmarks" / "requests.json",
+
+
     }
 ]
