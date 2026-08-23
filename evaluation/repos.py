3369b798133816cf5d9281a4f57341ef458b0460
Metodi Petrovski <149816710+MetodiPetrovski@users.noreply.github.com>
2026-08-23 20:40:09 +0200

    Update repos.py

--- evaluation/repos.py
+++ evaluation/repos.py
@@ -9,7 +9,7 @@ REPOSITORIES = [
   {
         "name": "strix",
         "url": "https://github.com/usestrix/strix.git",
-        "commit": "f528a6d26585b05bd55909394a8186fb851fc39a",
+        "commit": "e152c4c7c037a895b039d5fbdf469ac7b17a5ee9",
         "benchmark": BASE_DIR / "benchmarks" / "strix.json",
     }
 ]
